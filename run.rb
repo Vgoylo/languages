8190a7178e4327577ef9591208236331133a325e
vgoylo <y_draiv.by@mail.ru>
2021-09-27 10:19:41 +0300

    Add locale time

--- run.rb
+++ run.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require 'date'
 require_relative 'src/greeting'
 
 puts "Which is you language"
@@ -26,16 +27,32 @@ puts '4. Egypt'
 
 country = gets.chomp
 
+time = Time.now.utc
+year = time.year
+month = time.month
+day = time.day
+hour = time.hour
+min = time.min
+
+country_word, locale_hour = 
+  if country == '1'
+    ['Great Britan', hour - 1]
+  elsif country == '2'
+    ['Spain', hour + 2]
+  elsif country == '3'
+    ['Belarus', hour + 3]
+  elsif country == '4'
+    ["Egypt", hour + 11]
+  end
+
+puts "This is #{Time.new(year, month, day, locale_hour, min).inspect} in #{country_word}"
 
 
-country_word = if country == '1'
-  'Great Britan'
-elsif country == '2'
-  'Spain'
-elsif country == '3'
-  'Belarus'
-elsif country == '4'
-  "Egypt"
-end
 
-puts "this is time in #{country_word}"
+# def with_time_zone(&block)
+#   time_zone = current_user.time_zone
+#   logger.debug "Используется часовой пояс пользователя: #{time_zone}"
+#   Time.use_zone(time_zone, &block)
+# end
+
+
