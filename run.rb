dfeb49f7e27c449b80b59a90f80533ab42f92483
vgoylo <y_draiv.by@mail.ru>
2021-09-27 09:17:20 +0300

    Allow to input country

--- run.rb
+++ run.rb
@@ -2,6 +2,7 @@ require 'pry'
 require_relative 'src/greeting'
 
 puts "Which is you language"
+puts "Whre are you from"
 
 puts '1. English'
 puts '2. Espaniol'
@@ -16,3 +17,25 @@ end
 greeting = Greeting.new(result)
 
 puts greeting.hello
+
+
+puts '1. Great Britan'
+puts '2. Spain'
+puts '3. Belarus'
+puts '4. Egypt'
+
+country = gets.chomp
+
+
+
+country_word = if country == '1'
+  'Great Britan'
+elsif country == '2'
+  'Spain'
+elsif country == '3'
+  'Belarus'
+elsif country == '4'
+  "Egypt"
+end
+
+puts "this is time in #{country_word}"
